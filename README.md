af4f9a40ce292b9c0837e2520ec10aebfac1ff8d
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-02-21 15:21:54 +0200

    heroku

--- README.md
+++ README.md
@@ -3,16 +3,19 @@
 # Project Name
 
 **Author**: Bayan Abu Alhaj
-**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+**Version**: 1.0.0 
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+its a sever to get information on a city (location and weather) as the clint asked 
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+he should provide the programme with the name of the city and click explore
 
 ## Architecture
 <!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
 
 ## Change Log
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 let express= require('express');
+const cors = require('cors');
 
 let app= express();
+app.use(cors());
 
 //// this line is to order to use it every where 
 require('dotenv').config();
@@ -12,3 +14,51 @@ const PORT =process.env.PORT;
 app.listen(PORT,()=>{
     console.log('this app is listening on port 3000');
 })
+
+// routes -endpoints
+
+app.get('/location',handleLocation);
+// app.get('/weather',handleWeather);
+
+
+// great a call back function in order to be lent 
+
+function handleLocation(req,res){
+
+    let searchQuery= req.query.city;
+    console.log(searchQuery);
+    // I'm passing here the searchQuery to send the requst to the get location then in order to git the data we will make the response 
+    let locationObjects =getLocationData(searchQuery);
+
+    res.status(200).send(locationObjects);
+    
+}
+
+// get data ========
+
+function getLocationData(seaerchQuery){
+    ///this hoiw we came into the the json files 
+    let location = require('./data/location.json');
+    
+    /// because the location is an array i want to access the first index 
+    /// and because the first index is an object we use .lat .lon
+    let longitude=location[0].lon;
+    let latintude=location[0].lat;
+    let displayName=location[0].display_name;
+    
+    let responseObject = new CityLocation(seaerchQuery,displayName,latintude,longitude);
+
+    return responseObject;
+    // return to get the value to used in the handle because if I didn't do this there is will be no value 
+}
+
+
+// constructor to order the data 
+
+function CityLocation(searchQuery,displayName,lat,lon){
+    this.search_query=searchQuery;
+    this.formatted_query=displayName;
+    this.latitude=lat;
+    this.longitude=lon;
+
+}
