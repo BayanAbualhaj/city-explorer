0f2ef4ce02f2f5c16ad01f82309e0a321d12324b
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-02-24 20:41:24 +0200

    lab09

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ let superagent = require('superagent');
 let app= express();
 app.use(cors());
 const pg=require('pg'); 
+const { query } = require('express');
 require('dotenv').config();
 
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+const client = new pg.Client(process.env.DATABASE_URL);
 
 const PORT =process.env.PORT;
 
@@ -18,6 +20,8 @@ const PORT =process.env.PORT;
 app.get('/location',handleLocation);
 app.get('/weather',handleWeather);
 app.get('/parks',handlePark);
+app.get('/movies',handleMovies);
+app.get('/yelp',handleYelp);
 app.get('*',handle404);
 
 
@@ -49,7 +53,7 @@ function getLocationData(searchQuery,res){
 
     let url ='https://eu1.locationiq.com/v1/search.php';
     return superagent.get(url).query(query).then(data=>{
-        console.log(data.body);
+        // console.log(data.body);
         try{
             let longitude=data.body[0].lon;
             let latitude=data.body[0].lat;
@@ -57,16 +61,16 @@ function getLocationData(searchQuery,res){
             let formatName=data.body[0].display_name;
 
             let dbQuery =`INSERT INTO cities(cityName,formatName,lon,lat) VALUES ($1,$2,$3,$4)`;
-            console.log(longitude);
-            console.log(latitude);
-            console.log(name);
-            console.log(formatName);
+            // console.log(longitude);
+            // console.log(latitude);
+            // console.log(name);
+            // console.log(formatName);
             let safeValues=[name,formatName,longitude,latitude];
 
             client.query(dbQuery,safeValues).then(data=>{
-                console.log(data);
+                // console.log(data);
                 let resObject= new CityLocation(searchQuery,name,latitude,longitude);
-                console.log(resObject)
+                // console.log(resObject)
                 return resObject;
                 ;
             });
@@ -88,10 +92,10 @@ function checkDataBase(searchQuery,res){
                 res.status(200).send(data);
             });
         }else {
-            console.log(data.rows);
+            // console.log(data.rows);
             // in order to be lent creat var = data.rows[0]; in order to make it as a object to pass in the constructor
             let resObject= new CityLocation(data.rows[0].cityname,data.rows[0].formatname,data.rows[0].lat,data.rows[0].lon);
-            console.log('else ',resObject);
+            // console.log('else ',resObject);
             res.status(200).send(resObject);
         }
     });
@@ -254,9 +258,135 @@ function Parks(name,address,fee,description,url){
 }
 
 
-//======================================
+//=================================================Movies========================================
+
+function handleMovies(req,res){
+    
+    getMoviesData(req,res).then(data=>{
+        res.status(200).send(data);
+    });
+}
+
+//=======================Get Movies Data=============================
+
+function getMoviesData(req,res){
+    let url= 'https://api.themoviedb.org/3/search/movie';
+
+    const query={
+        api_key:process.env.MOVIE_API_KEY,
+        query:req.query.search_query
+    }
+
+    return superagent.get(url).query(query).then(data=>{
+        try{
+                // console.log(data.body.results);
+                let movieArray=[]; 
+                data.body.results.map((value)=>{
+
+                    let title=value.title;
+                    let overview=value.overview;
+                    let averageVotes=value.vote_average;
+                    let totalVotes=value.vote_count;
+                    let imageURL=value.poster_path;
+                    let popularity=value.popularity;
+                    let releasedOn=value.release_date;
+
+                    let moviesObject= new Movies(title,overview,averageVotes,totalVotes,imageURL,popularity,releasedOn);
+                    // console.log(moviesObject);
+
+                    movieArray.push(moviesObject);
+            
+                });
+                // console.log(movieArray);
+        return movieArray;
+        }catch(error){
+            console.log('an error found inside the superagint ',error);
+        }
+    }).catch(error=>{
+        console.log('error had accourd in the super agint ', error)
+    });
+}
+
+//==============Movie Constructor=====================
+
+function Movies(title,overview,averageVotes,totalVotes,imageURL,popularity,releasedOn){
+
+    this.title=title;
+    this.overview=overview;
+    this.average_votes=averageVotes;
+    this.total_votes=totalVotes;
+    this.image_url='https://image.tmdb.org/t/p/w500'+imageURL;
+    this.popularity=popularity;
+    this.released_on=releasedOn;
+
+}
+
+//=========================================Yelp====================================================
 
 
+function handleYelp(req,res){
+    getYelpData(req, res).then(data=>{
+        res.status(200).send(data);
+
+    });
+}
+
+//===============Get Yelp data=================
+
+function getYelpData(req,res){
+    let url='https://api.yelp.com/v3/businesses/search';
+
+
+    let x=0;
+    let y=req.query.page;
+    
+
+
+    const query={
+        location:req.query.search_query,
+        limit:5,
+        offset:x+(5*y),
+    }
+
+    return superagent.get(url).set('Authorization','Bearer sM7CFpPMC-Ce-BmvPQW1B8TxQt5R8ogR8yz7lkzkCs1pBXrU1nQkY8aQerUs0sO52Ic3S3yZbHV1OKu556ZMaluks4MS2SE4ck8i9H11tz84c6OQpidJrb2bTFE2YHYx').query(query).then(data=>{
+
+        try{
+        // console.log(data.body);
+        let arrayYelp=[];
+        data.body.businesses.map(value=>{
+
+                let name=value.name;
+                let image_url=value.image_url;
+                let price=value.price;
+                let rating=value.rating;
+                let url=value.url;
+
+                let yelpObject= new Yelp(name,image_url,price,rating,url);
+                arrayYelp.push(yelpObject);
+                
+            });
+            return arrayYelp;
+             }catch(error){
+            console.log('error had been dectected .........',error);
+        }
+    }).catch(error=>{
+        console.log('there is an error inside the superagint',error);
+    });
+
+}
+
+//=============================Yelp constractor==============
+
+function Yelp(name,image_url,price,rating,url){
+    this.name=name;
+    this.image_url=image_url;
+    this.price=price;
+    this.rating=rating;
+    this.url=url;
+}
+
+
+//=====================================================================================================
 client.connect().then(()=>{
     app.listen(PORT,()=>{
     console.log('this app is listening on port 3000');
